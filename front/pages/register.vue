14024210b32c264bb4c641afffa1b22686ccb5fb
HuntingRathalos <huntingandrathalos@gmail.com>
2022-03-22 21:42:14 +0900

    画面作成,会員登録機能実装(signUp)2 #4

--- front/pages/register.vue
+++ front/pages/register.vue
@@ -64,7 +64,7 @@ export default {
           required: value => !!value || '必須項目なので値を入力してください。',
           email: value => {
             const pattern = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-            return pattern.test(value) || '正しい形式のメールアドレスを入力してください。'
+            return pattern.test(value) || '正しい形式で入力してください。'
           },
         },
     };
