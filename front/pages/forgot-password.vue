4f8c67b2f0d5c35f636263f2d47305cbb1e7566b
HuntingRathalos <huntingandrathalos@gmail.com>
2022-03-26 16:09:03 +0900

    パスワードリセット機能のバリデーション修正 #10

--- front/pages/forgot-password.vue
+++ front/pages/forgot-password.vue
@@ -4,10 +4,13 @@
       <h1 class="display-1">パスワードリセット</h1>
     </v-card-title>
     <v-card-text>
-      <v-form @submit.prevent="sendForgotPasswordEmail">
+      <v-form
+        ref="forgot_password_form"
+        @submit.prevent="sendForgotPasswordEmail"
+      >
         <v-text-field
           v-model="form.email"
-          :rules="[rules.required, rules.email]"
+          :rules="[rules.required, rules.email, rules.maxCount255]"
           prepend-icon="mdi-email-outline"
           label="メールアドレス"
           type="email"
@@ -31,6 +34,8 @@ export default {
       },
       rules: {
         required: (value) => !!value || '必須項目なので値を入力してください。',
+        maxCount255: (value) =>
+          value.length <= 255 || '文字数をオーバーしています。',
         email: (value) => {
           const pattern =
             /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
@@ -44,14 +49,16 @@ export default {
   },
   methods: {
     async sendForgotPasswordEmail() {
-      await this.$axios.get('sanctum/csrf-cookie')
-      const response = await this.$axios
-        .post('forgot-password', this.form)
-        .then(console.log())
-        .catch((err) => err.response || err)
+      if (this.$refs.forgot_password_form.validate()) {
+        await this.$axios.get('sanctum/csrf-cookie')
+        const response = await this.$axios
+          .post('forgot-password', this.form)
+          .then(console.log())
+          .catch((err) => err.response || err)
 
-      if (response.status === 400) {
-        console.log(response)
+        if (response.status === 400) {
+          console.log(response)
+        }
       }
     }
   }
