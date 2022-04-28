3893933e1c4bd13682d98bd4df6fdbfacc84d103
HuntingRathalos <huntingandrathalos@gmail.com>
2022-04-28 16:07:41 +0900

    コンポーネントの実装(SimpleCard) #20

---
+++ front/components/molecules/cards/SimpleCard.vue
@@ -0,0 +1,12 @@
+<template>
+  <v-card outlined>
+    <v-card-text class="black--text">
+      <div class="text-body-1"><slot></slot></div>
+      <div class="text-h6 font-weight-medium">30000円</div>
+      <div class="text-body-2 text--secondary">目標まで :20000円</div>
+    </v-card-text>
+  </v-card>
+</template>
+<script>
+export default {}
+</script>
---
+++ front/pages/home.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="">
+    <v-row>
+      <v-col cols="12">
+        <v-card outlined>
+          <v-card-title>目標 旅行</v-card-title>
+        </v-card>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col cols="6">
+        <SimpleCard>目標額</SimpleCard>
+      </v-col>
+      <v-col cols="6">
+        <SimpleCard>貯金額</SimpleCard>
+      </v-col>
+    </v-row>
+  </div>
+</template>
+<script>
+import SimpleCard from '../components/molecules/cards/SimpleCard.vue'
+export default {
+  name: 'HomePage',
+  components: { SimpleCard }
+}
+</script>
