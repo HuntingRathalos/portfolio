29f73bc6b1c2513b66f0c28fad244ff00f108322
HuntingRathalos <huntingandrathalos@gmail.com>
2022-05-02 17:10:54 +0900

    TargetRequest、TargetRequestTest実装 #20

---
+++ api/tests/Unit/Requests/TargetRequestTest.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace Tests\Unit\Requests;
+
+use Tests\TestCase;
+use Illuminate\Support\Facades\Validator;
+use App\Http\Requests\TargetRequest;
+use Illuminate\Support\Str;
+
+class TargetRequestTest extends TestCase
+{
+
+  /**
+     * @test
+     * @dataProvider validationProvider
+     *
+     * テストの期待値
+     * @param bool $expected
+     * フォームリクエストのモックデータ
+     * @param array $data
+     */
+    public function バリデーションが通るか(bool $expected, array $data)
+    {
+        // フォームリクエストで定義したルールを取得
+        $rules = (new TargetRequest())->rules();
+
+        // Validatorファサードでバリデーターインスタンス取得、引数に入力情報とルールを渡す
+        $validator = Validator::make($data, $rules);
+
+        $this->assertEquals($expected, $validator->passes());
+    }
+
+    public function validationProvider()
+    {
+        return [
+            'OK' => [
+                'expected' => true,
+                'data' => [
+                    'name' => 'お菓子買う',
+                    'amount' => 30000,
+                ]
+            ],
+            'name必須エラー' => [
+                'expected' => false,
+                'data' => [
+                    'name' => null,
+                    'amount' => 30000,
+                ]
+            ],
+            'name最大文字数エラー' => [
+                'expected' => false,
+                'data' => [
+                    'name' => Str::random(51),
+                    'amount' => 30000,
+                ]
+            ],
+            'amount必須エラー' => [
+                'expected' => false,
+                'data' => [
+                    'name' => 'お菓子買う',
+                    'amount' => null,
+                ]
+            ],
+        ];
+    }
+}
