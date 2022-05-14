3a417d29886ac7bdbe3bf179bad0ad05662fc2dc
HuntingRathalos <huntingandrathalos@gmail.com>
2022-05-15 01:18:17 +0900

    follower-profile.vueのデザイン修正 #27

--- front/components/organisms/lists/FollowerProfileList.vue
+++ front/components/organisms/lists/FollowerProfileList.vue
@@ -1,36 +1,38 @@
 <template>
-  <v-card v-ripple class="mx-auto" max-width="400">
-    <v-list-item two-line>
-      <v-list-item-content>
-        <v-list-item-title class="text-h5">
-          {{ followUser.name }}
-        </v-list-item-title>
-      </v-list-item-content>
-    </v-list-item>
-    <v-list class="transparent" dense>
-      <v-list-item>
-        <v-list-item-title>目標</v-list-item-title>
-
-        <v-list-item-subtitle class="text-center ml-12">
-          {{ followUser.target }}
-        </v-list-item-subtitle>
+  <div>
+    <v-card class="pl-2 mx-auto" :class="marginClass" :width="cardWidth">
+      <v-list-item two-line>
+        <v-list-item-content>
+          <v-list-item-title class="text-h5">
+            {{ followUser.name }}
+          </v-list-item-title>
+        </v-list-item-content>
       </v-list-item>
-      <v-list-item>
-        <v-list-item-title>目標金額</v-list-item-title>
+      <v-list class="transparent" dense>
+        <v-list-item>
+          <v-list-item-title>目標</v-list-item-title>
 
-        <v-list-item-subtitle class="text-center ml-12">
-          {{ followUser.targetAmount }}
-        </v-list-item-subtitle>
-      </v-list-item>
-      <v-list-item>
-        <v-list-item-title>最初に我慢したもの</v-list-item-title>
+          <v-list-item-subtitle class="text-center ml-12">
+            {{ followUser.target }}
+          </v-list-item-subtitle>
+        </v-list-item>
+        <v-list-item>
+          <v-list-item-title>目標金額</v-list-item-title>
 
-        <v-list-item-subtitle class="text-center ml-12">
-          {{ followUser.tagName }}
-        </v-list-item-subtitle>
-      </v-list-item>
-    </v-list>
-  </v-card>
+          <v-list-item-subtitle class="text-center ml-12">
+            {{ followUser.targetAmount }}円
+          </v-list-item-subtitle>
+        </v-list-item>
+        <v-list-item>
+          <v-list-item-title>最初に我慢したもの</v-list-item-title>
+
+          <v-list-item-subtitle class="text-center ml-12">
+            {{ followUser.tagName }}
+          </v-list-item-subtitle>
+        </v-list-item>
+      </v-list>
+    </v-card>
+  </div>
 </template>
 <script>
 export default {
@@ -38,10 +40,23 @@ export default {
     followUser: {
       type: Object,
       default: null
+    },
+    marginClass: {
+      type: String,
+      default: 'mb-0'
     }
   },
   data() {
     return {}
+  },
+  computed: {
+    cardWidth() {
+      const brackPointName = this.$vuetify.breakpoint.name
+      if (brackPointName === 'xs') {
+        return '340'
+      }
+      return '400'
+    }
   }
 }
 </script>
