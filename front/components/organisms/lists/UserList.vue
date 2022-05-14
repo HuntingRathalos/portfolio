b2ac0b1ccfa032fae9bdd0f758c94d209927b264
HuntingRathalos <huntingandrathalos@gmail.com>
2022-05-14 21:49:55 +0900

    users.vueのデザイン修正、ユーザー検索機能実装 #27

--- front/components/organisms/lists/UserList.vue
+++ front/components/organisms/lists/UserList.vue
@@ -1,12 +1,35 @@
 <template>
-  <v-list subheader two-line>
-    <user-list-item
-      v-for="user in users"
-      :key="user.id"
-      :user="user"
-      :follow-users-id="followUsersId"
-    />
-  </v-list>
+  <div>
+    <div style="max-width: 400px" class="mx-auto">
+      <v-text-field
+        v-model="keyword"
+        color="indigo accent-2"
+        prepend-icon="mdi-magnify"
+        placeholder="ユーザー名を入力"
+        solo
+        outlined
+        dense
+        rounded
+      />
+    </div>
+    <div class="text-right">
+      <v-btn to="/follower-profile" text color="blue" nuxt class="ml-auto">
+        フォローユーザーのプロフィールへ
+        <v-icon> mdi-chevron-right </v-icon>
+      </v-btn>
+    </div>
+    <v-card max-width="800" class="mx-auto">
+      <v-list subheader two-line class="pa-0" outlined>
+        <v-subheader> ユーザーリスト </v-subheader>
+        <user-list-item
+          v-for="user in filteredUsers"
+          :key="user.id"
+          :user="user"
+          :follow-users-id="followUsersId"
+        />
+      </v-list>
+    </v-card>
+  </div>
 </template>
 <script>
 import UserListItem from '../../molecules/listItem/UserListItem.vue'
@@ -14,8 +37,17 @@ export default {
   components: { UserListItem },
   data() {
     return {
+      keyword: '',
       users: [],
       followUsersId: []
+    }
+  },
+  computed: {
+    filteredUsers() {
+      const users = this.users.filter(
+        (user) => user.name.includes(this.keyword) !== false
+      )
+      return users
     }
   },
   created() {
