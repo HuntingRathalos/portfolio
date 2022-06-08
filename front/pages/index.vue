bdeecb630af151d4a0f2b360a2ae371c0bf66e18
HuntingRathalos <huntingandrathalos@gmail.com>
2022-06-08 12:18:53 +0900

    画像変更 #43

--- front/pages/index.vue
+++ front/pages/index.vue
@@ -3,7 +3,7 @@
     <v-row align="center">
       <v-col cols="12" md="6" class="text-right ml-auto">
         <div class="text-h3 font-weight-bold mt-6 mb-6">
-          継続できる500円貯金<br />始めませんか。
+          継続できる500円貯金<br />始めてみませんか。
         </div>
         <div class="text-h6 font-weight-bold">
           手軽にあなたの継続を可視化できる<br />貯金記録サービス
@@ -11,49 +11,62 @@
       </v-col>
       <v-col cols="12" md="6" class="d-flex justify-center">
         <v-img
-          :src="require('@/assets/endure_person.png')"
+          :src="require('@/assets/Piggy bank-bro.svg')"
           max-height="400"
           max-width="400"
         >
         </v-img>
       </v-col>
     </v-row>
-    <div class="grey lighten-2">
-      <v-container>
-        <v-row>
-          <v-col cols="12" md="10">
-            <div class="text-body-1">貯金した日をカレンダーに記録</div>
-            <div class="text-body-2">
-              カレンダーに印がつくのですぐに振り返ることができます。
-            </div>
-          </v-col>
-          <v-col cols="12" md="2">
-            <v-img :src="require('@/assets/endure_person.png')"> </v-img>
-          </v-col>
-        </v-row>
-        <v-row>
-          <v-col cols="12" md="2">
-            <v-img :src="require('@/assets/endure_person.png')"> </v-img>
-          </v-col>
-          <v-col cols="12" md="10">
-            <div class="text-body-1">グラフによる「可視化」</div>
-            <div class="text-body-2">
-              ２つのグラフによる「可視化」があなたの継続を手伝います。
-            </div>
-          </v-col>
-        </v-row>
-        <v-row>
-          <v-col cols="12" md="10">
-            <div class="text-body-1">ニュース閲覧</div>
-            <div class="text-body-2">
-              あなたがアプリを開く習慣を身につけることを手伝います。
-            </div>
-          </v-col>
-          <v-col cols="12" md="2">
-            <v-img :src="require('@/assets/endure_person.png')"> </v-img>
-          </v-col>
-        </v-row>
-      </v-container>
+    <div class="grey lighten-2" style="max-width: 800px">
+      <v-row>
+        <v-col cols="12" md="10">
+          <div class="text-h6">貯金した日をカレンダーに記録</div>
+          <div class="text-body-2">
+            カレンダーに印がつくのですぐに振り返ることができます。
+          </div>
+        </v-col>
+        <v-col cols="12" md="2">
+          <v-img
+            :src="require('@/assets/Date picker-rafiki.svg')"
+            max-height="400"
+            max-width="400"
+          >
+          </v-img>
+        </v-col>
+      </v-row>
+      <v-row class="d-md-flex flex-md-column-reverse">
+        <v-col cols="12" md="2">
+          <v-img
+            :src="require('@/Pie chart-rafiki.svg')"
+            max-height="400"
+            max-width="400"
+          >
+          </v-img>
+        </v-col>
+        <v-col cols="12" md="10">
+          <div class="text-h6">グラフによる「可視化」</div>
+          <div class="text-body-2">
+            ２つのグラフによる「可視化」があなたの継続を手伝います。
+          </div>
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col cols="12" md="10">
+          <div class="text-h6">ニュース閲覧機能</div>
+          <div class="text-body-2">
+            あなたがアプリを開く習慣を身につけることができます。
+          </div>
+        </v-col>
+        <v-col cols="12" md="2">
+          <v-img
+            :src="require('@/assets/Exciting news-rafiki.svg')"
+            max-height="400"
+            max-width="400"
+          >
+          </v-img>
+        </v-col>
+      </v-row>
     </div>
   </div>
 </template>
