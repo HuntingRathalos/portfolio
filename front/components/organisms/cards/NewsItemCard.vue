c7a60198c45e46a3395cb9e4559641d615e65c29
HuntingRathalos <huntingandrathalos@gmail.com>
2022-06-10 13:34:17 +0900

    NewsItemCard.vue実装 #45

---
+++ front/components/organisms/cards/NewsItemCard.vue
@@ -0,0 +1,33 @@
+<template>
+  <v-card>
+    <v-img
+      height="220"
+      src="https://cdn.vuetifyjs.com/images/cards/cooking.png"
+    ></v-img>
+
+    <v-card-title class="justify-center">Title</v-card-title>
+
+    <v-card-text>
+      <div class="text-body-1">
+        Content
+        {{ news.id }}
+      </div>
+    </v-card-text>
+
+    <v-card-actions class="justify-center">
+      <v-btn color="deep-purple lighten-2" text :href="記事のurl">
+        もっと見る
+      </v-btn>
+    </v-card-actions>
+  </v-card>
+</template>
+<script>
+export default {
+  props: {
+    news: {
+      type: Object,
+      default: null
+    }
+  }
+}
+</script>
---
+++ front/pages/news/_category/index.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="">
+    <div class="text-left">
+      <v-btn to="/news" text color="blue" nuxt class="mr-auto">
+        <v-icon> mdi-chevron-left </v-icon>
+        カテゴリー選択へ
+      </v-btn>
+    </div>
+    <v-row justify="center">
+      <v-col
+        v-for="news in newsArray"
+        :key="news.id"
+        cols="12"
+        sm="6"
+        md="4"
+        lg="3"
+      >
+        <news-item-card :news="news" />
+      </v-col>
+    </v-row>
+  </div>
+</template>
+<script>
+import NewsItemCard from '../../../components/organisms/cards/NewsItemCard.vue'
+export default {
+  name: 'NewsList',
+  components: { NewsItemCard },
+  auth: false,
+  data() {
+    return {
+      newsArray: [
+        {
+          id: 1
+        },
+        {
+          id: 2
+        },
+        {
+          id: 3
+        },
+        {
+          id: 4
+        },
+        {
+          id: 5
+        },
+        {
+          id: 6
+        }
+      ]
+    }
+  }
+  // methods: {
+  //   goNewsDetail() {
+  //     const category = this.$route.params.category
+  //     this.$router.push({ path: `/news/${category}/${id}`})
+  //     this.$router.push({ path: `/news/${category}/1`})
+  //   }
+  // }
+}
+</script>
