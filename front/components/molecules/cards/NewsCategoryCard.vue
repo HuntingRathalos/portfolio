4b5e1620971a9e9d82e490a29d1cab62de999f32
HuntingRathalos <huntingandrathalos@gmail.com>
2022-06-10 13:32:55 +0900

    NewsCategoryCard.vue実装 #45

---
+++ front/components/molecules/cards/NewsCategoryCard.vue
@@ -0,0 +1,36 @@
+<template>
+  <v-card
+    class="mx-auto mb-3"
+    :color="news.color"
+    dark
+    elevation="3"
+    outlined
+    max-width="400px"
+    @click="goNewsList"
+  >
+    <v-card-title>
+      <v-icon large left>
+        {{ news.icon }}
+      </v-icon>
+      <span class="text-h6 font-weight-light">
+        {{ news.category }}
+      </span>
+    </v-card-title>
+  </v-card>
+</template>
+<script>
+export default {
+  props: {
+    news: {
+      type: Object,
+      default: null
+    }
+  },
+  methods: {
+    goNewsList() {
+      const category = this.news.category
+      this.$router.push({ path: `/news/${category}` })
+    }
+  }
+}
+</script>
---
+++ front/pages/news/index.vue
@@ -0,0 +1,53 @@
+<template>
+  <div>
+    <news-category-card
+      v-for="news in newsArray"
+      :key="news.category"
+      :news="news"
+    />
+  </div>
+</template>
+<script>
+import NewsCategoryCard from '../../components/molecules/cards/NewsCategoryCard.vue'
+export default {
+  name: 'NewsPage',
+  components: { NewsCategoryCard },
+  auth: false,
+  data() {
+    return {
+      newsArray: [
+        {
+          color: '#f87979',
+          icon: 'mdi-handshake-outline',
+          category: 'ビジネス'
+        },
+        {
+          color: '#303F9F',
+          icon: 'mdi-robot-happy-outline',
+          category: 'エンターテイメント'
+        },
+        {
+          color: '#80DEEA',
+          icon: 'mdi-bottle-tonic-plus-outline',
+          category: '健康'
+        },
+        {
+          color: '#66BB6A',
+          icon: 'mdi-flask-outline',
+          category: '科学'
+        },
+        {
+          color: '#FFCA28',
+          icon: 'mdi-ski',
+          category: 'スポーツ'
+        },
+        {
+          color: '#B388FF',
+          icon: 'mdi-robot',
+          category: 'テクノロジー'
+        }
+      ]
+    }
+  }
+}
+</script>
