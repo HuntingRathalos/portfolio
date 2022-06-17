2af28713dfc7b2a700697d9c482bb7a3e38501a4
HuntingRathalos <huntingandrathalos@gmail.com>
2022-06-17 20:49:00 +0900

    timeline.vue実装 #47

---
+++ front/pages/timeline.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="">
+    <ul>
+    <li
+      v-for="post in posts"
+      :key="post.id"
+      @click="closeModal(post.id)"
+    >
+      {{ item.name }}
+    </li>
+</ul>
+  </div>
+</template>
+<script>
+import { mapGetters, mapActions } from 'vuex'
+export default {
+  name: 'TimelinePage',
+  data() {
+    return {
+      postId: '',
+    }
+  },
+    computed: {
+    ...mapGetters({
+      posts: 'post/posts'
+    })
+  },
+  methods: {
+     ...mapActions({
+      createPost: 'post/createPost'
+    }),
+    closeModal(id) {
+      console.log(id)
+      this.postId = id
+      console.log(this.postId)
+      this.createPost(this.postId)
+    }
+  }
+}
+</script>
---
+++ front/store/post.js
@@ -0,0 +1,61 @@
+export const state = () => ({
+  posts: [
+    {
+      id: 1,
+      name: '雨'
+    },
+    {
+      id: 2,
+      name: 'とり'
+    },
+    {
+      id: 3,
+      name: '草'
+    },
+  ],
+  likePosts: [],
+})
+
+export const getters = {
+  posts: (state) => state.posts,
+}
+
+export const mutations = {
+  createPost (state, payload) {
+    console.log(payload)
+    state.posts.unshift(payload)
+  },
+  updatePost (state, payload) {
+    const post = state.posts.find(
+      (post) => post.id === payload.id
+    )
+    Object.assign(post, payload)
+  },
+  deletePost (state, payload) {
+    state.posts.filter((post) => post.id !== payload.id)
+  },
+  likePost (state, payload) {
+    state.likePosts.unshift(payload)
+  },
+  unlikePost (state, payload) {
+    state.likePosts.filter((likePost) => likePost.id !== payload.id)
+  },
+}
+
+export const actions = {
+  createPost ({ commit }, payload) {
+    commit('createPost', payload)
+  },
+  updatePost ({ commit }, payload) {
+    commit('updatePost', payload)
+  },
+  deletePost ({ commit }, payload) {
+    commit('deletePost', payload)
+  },
+  likePost ({ commit }, payload) {
+    commit('likePost', payload)
+  },
+  unlikePost ({ commit }, payload) {
+    commit('likePost', payload)
+  },
+}
