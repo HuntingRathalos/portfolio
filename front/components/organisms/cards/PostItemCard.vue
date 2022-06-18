5e19da6d411200b6a019089843d6a193e8e46c65
HuntingRathalos <huntingandrathalos@gmail.com>
2022-06-18 22:30:11 +0900

    PostItemCard, PoatLikeCard実装 #47

---
+++ front/components/organisms/cards/PostItemCard.vue
@@ -0,0 +1,85 @@
+<template>
+  <v-card class="mx-auto" flat>
+    <v-card flat class="text-center pt-2">
+      <!-- <h2 class="font-weight-medium">pooさん</h2> -->
+      <h2 class="font-weight-medium">authモジュールのやつ</h2>
+      <h4 class="font-weight-medium">{{ post.created_at }}</h4>
+    </v-card>
+    <v-card-actions class="mb-2">
+      <v-row>
+        <v-col cols="12">
+          <span class="text-subtitle-1 font-weight-bold text--secondary pl-2"
+            >自己評価</span
+          >
+          <v-row justify="center">
+            <v-rating
+              background-color="orange lighten-3"
+              color="orange"
+              large
+              length="5"
+              size="64"
+              :value="post.self_evaluation"
+            ></v-rating>
+          </v-row>
+        </v-col>
+      </v-row>
+    </v-card-actions>
+    <v-card-actions>
+      <v-row class="mx-auto">
+        <v-col cols="12">
+          <v-textarea
+            autofocus
+            readonly
+            success
+            color="green accent-3"
+            label="良かったポイント"
+            :value="post.good_description"
+            rows="3"
+          ></v-textarea>
+        </v-col>
+      </v-row>
+    </v-card-actions>
+    <v-card-actions>
+      <v-row class="mx-auto">
+        <v-col cols="12">
+          <v-textarea
+            readonly
+            error
+            color="red accent-2"
+            label="わるかったポイント"
+            :value="post.bad_description"
+            rows="3"
+          ></v-textarea>
+        </v-col>
+      </v-row>
+    </v-card-actions>
+
+    <v-card-actions class="ml-auto">
+      <div class="ml-auto">
+        <v-btn icon text color="grey darken-2">
+          <v-icon> mdi-pencil-box-multiple </v-icon>
+        </v-btn>
+        <v-btn icon text color="grey darken-2" @click="deletePost">
+          <v-icon> mdi-delete </v-icon>
+        </v-btn>
+        <v-btn icon text color="grey darken-2" @click="likePost">
+          <v-icon> mdi-heart </v-icon>
+        </v-btn>
+      </div>
+    </v-card-actions>
+    <v-divider></v-divider>
+  </v-card>
+</template>
+<script>
+export default {
+  props: {
+    post: {
+      type: Object,
+      default: null
+    }
+  },
+  methods: {
+    likePost() {}
+  }
+}
+</script>
--- front/pages/timeline.vue
+++ front/pages/timeline.vue
@@ -10,98 +10,50 @@
         slider-color="orange"
         icons-and-text
       >
-        <v-tab class="ma-0 pa-0" href="#tab-1">
+        <v-tab class="ma-0 pa-0" href="#post_list">
           新着順
           <v-icon>mdi-clock-time-eight</v-icon>
         </v-tab>
-        <v-tab class="ma-0 pa-0" href="#tab-2">
+        <v-tab class="ma-0 pa-0" href="#post_like">
           いいね
-          <v-icon>mdi-clock-time-eight</v-icon>
+          <v-icon>mdi-heart-settings</v-icon>
         </v-tab>
       </v-tabs>
       <v-tabs-items v-model="tab">
-        <v-tab-item v-for="i in 3" :key="i" :value="'tab-' + i" class="pa-1">
-          <v-card class="mx-auto" flat>
-            <v-card flat class="text-center pt-2">
-              <h2 class="font-weight-medium">pooさん</h2>
-              <h4 class="font-weight-medium">2022年06183時23分</h4>
-            </v-card>
-            <v-card-actions class="mb-2">
-              <v-row>
-                <v-col cols="12">
-                  <span
-                    class="text-subtitle-1 font-weight-bold text--secondary pl-2"
-                    >自己評価</span
-                  >
-                  <v-row justify="center">
-                    <v-rating
-                      background-color="orange lighten-3"
-                      color="orange"
-                      large
-                      length="5"
-                      size="64"
-                    ></v-rating>
-                  </v-row>
-                </v-col>
-              </v-row>
-            </v-card-actions>
-            <v-card-actions>
-              <v-row class="mx-auto">
-                <v-col cols="12">
-                  <v-textarea
-                    autofocus
-                    readonly
-                    success
-                    color="green accent-3"
-                    label="良かったポイント"
-                    value="お菓子を我慢できた"
-                    rows="3"
-                  ></v-textarea>
-                </v-col>
-              </v-row>
-            </v-card-actions>
-            <v-card-actions>
-              <v-row class="mx-auto">
-                <v-col cols="12">
-                  <v-textarea
-                    readonly
-                    error
-                    color="red accent-2"
-                    label="わるかったポイント"
-                    value="お菓子を我慢できなかった"
-                    rows="3"
-                  ></v-textarea>
-                </v-col>
-              </v-row>
-            </v-card-actions>
-
-            <v-card-actions class="ml-auto">
-              <div class="ml-auto">
-                <v-btn icon text color="grey darken-2">
-                  <v-icon> mdi-pencil-box-multiple </v-icon>
-                </v-btn>
-                <v-btn icon text color="grey darken-2">
-                  <v-icon> mdi-delete </v-icon>
-                </v-btn>
-                <v-btn icon text color="grey darken-2">
-                  <v-icon> mdi-heart </v-icon>
-                </v-btn>
-              </div>
-            </v-card-actions>
-            <v-divider></v-divider>
-          </v-card>
+        <v-tab-item class="pa-1">
+          <post-item-card
+            v-for="post in posts"
+            :key="post.id"
+            :post="post"
+            @post-id-send="sendPostId(post.id)"
+          />
+        </v-tab-item>
+        <v-tab-item class="pa-1">
+          <post-like-card />
         </v-tab-item>
       </v-tabs-items>
     </v-col>
   </v-row>
 </template>
 <script>
+import PostItemCard from '../components/organisms/cards/PostItemCard.vue'
+import PostLikeCard from '../components/organisms/cards/PostLikeCard.vue'
 export default {
-  name: 'AaaList',
+  name: 'TimelinePage',
+  components: { PostItemCard, PostLikeCard },
   auth: false,
   data() {
     return {
-      tab: null
+      tab: 'post_list',
+      postId: null
+    }
+  },
+  methods: {
+    sendPostId(id) {
+      this.postId = id
+      // モーダルにpropsで渡す
+      // モーダルをひらく
+      // モーダルでid番目のpostを取得して反映させる
     }
   }
 }
