7f4bc63324a599a48d6602df953a7d2d5de35d38
HuntingRathalos <huntingandrathalos@gmail.com>
2022-06-18 22:35:04 +0900

    plugins/modules/post.js実装 #47

---
+++ front/plugins/axios/modules/post.js
@@ -0,0 +1,42 @@
+export default ({ $axios }, inject) => {
+  const postApi = new PostApi($axios)
+  inject('postApi', postApi)
+}
+
+class PostApi {
+  constructor(axios) {
+    this.axios = axios
+  }
+
+  async get() {
+    return await this.axios.get('api/posts')
+  }
+
+  async getPostById(id) {
+    return await this.axios.get(`api/posts${id}`)
+  }
+
+  // async getLikePosts() {
+  //   return await this.axios.get('api/posts/')
+  // }
+
+  async create(post) {
+    return await this.axios.post('api/posts', post)
+  }
+
+  async update(id, post) {
+    return await this.axios.patch(`api/posts/${id}`, post)
+  }
+
+  async delete(id) {
+    return await this.axios.delete(`api/posts/${id}`)
+  }
+
+  async like(id) {
+    return await this.axios.patch(`api/posts/like/${id}`)
+  }
+
+  async unlike(id) {
+    return await this.axios.delete(`api/posts/like/${id}`)
+  }
+}
