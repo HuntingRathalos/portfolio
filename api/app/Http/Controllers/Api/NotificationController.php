a234d9d78e869c2fb0117e82dbc8c91a5f39de9a
HuntingRathalos <huntingandrathalos@gmail.com>
2022-07-13 20:30:10 +0900

    NotificationController実装 #57

---
+++ api/app/Http/Controllers/Api/NotificationController.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Services\Notification\NotificationServiceInterface;
+
+
+class NotificationController extends Controller
+{
+    private $notificationService;
+
+    /**
+     * @param NotificationServiceInterface $notificationService
+     */
+    public function __construct(NotificationServiceInterface $notificationService)
+    {
+        $this->notificationService = $notificationService;
+    }
+
+    /**
+     *ユーザーに紐づく通知を取得
+     *
+     * @return void
+     */
+    public function index()
+    {
+        return $this->notificationService->getNotifications();
+    }
+
+    /**
+     *既読履歴をつける
+     *
+     * @param int $notificationId
+     * @return void
+     */
+    public function read(int $notificationId)
+    {
+        return $this->notificationService->readNotification($notificationId);
+    }
+    // /**
+    //  *既読履歴をつける
+    //  *
+    //  * @param DatabaseNotification $notification
+    //  * @return void
+    //  */
+    // public function read(DatabaseNotification $notification)
+    // {
+    //     return $this->notificationService->readNotification();
+    // }
+    // /**
+    //  *既読履歴を外す
+    //  *
+    //  * @return void
+    //  */
+    // public function unread(DatabaseNotification $notification)
+    // {
+    //     return $this->notificationService->unreadNotification();
+    // }
+
+    /**
+     *通知を削除する
+     *
+     * @return void
+     */
+    public function delete(int $notificationId)
+    {
+        return $this->notificationService->deleteNotification($notificationId);
+    }
+}
